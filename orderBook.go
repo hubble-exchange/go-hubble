84e44a5d6c24c3922727d568d401f55b093a2ca0
Shubham Goyal <sgshubham@gmail.com>
2023-06-18 17:03:31 +0530

    Add 5 basic functions

---
+++ orderBook.go
@@ -0,0 +1,59 @@
+package hubble
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/hubble-exchange/go-hubble/contracts"
+)
+
+var OrderBookContractAddress = common.HexToAddress("0x0300000000000000000000000000000000000000")
+
+func placeOrder(txOptions *bind.TransactOpts, order contracts.IOrderBookOrder) (common.Hash, error) {
+	rpcEndpoint := os.Getenv("RPC_ENDPOINT")
+	client, err := ethclient.Dial(rpcEndpoint)
+	if err != nil {
+		return common.Hash{}, err
+	}
+
+	instance, err := contracts.NewOrderBook(OrderBookContractAddress, client)
+	if err != nil {
+		return common.Hash{}, err
+	}
+
+	orderHash_, err := instance.GetOrderHash(&bind.CallOpts{}, order)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	orderHash := common.Hash(orderHash_)
+	_, err = instance.PlaceOrder(txOptions, order)
+	if err != nil {
+		return orderHash, err
+	}
+
+	fmt.Println("placed order")
+	return orderHash, nil
+}
+
+func cancelOrder(txOptions *bind.TransactOpts, orders []contracts.IOrderBookOrder) error {
+	rpcEndpoint := os.Getenv("RPC_ENDPOINT")
+	client, err := ethclient.Dial(rpcEndpoint)
+	if err != nil {
+		return err
+	}
+
+	instance, err := contracts.NewOrderBook(OrderBookContractAddress, client)
+	if err != nil {
+		return err
+	}
+
+	_, err = instance.CancelOrders(txOptions, orders)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
